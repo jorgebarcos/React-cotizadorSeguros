0559d66663d7d32e396a6ef6658b04381fa68f2f
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-19 13:04:45 -0300

    Validando el Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -39,8 +39,18 @@ const Boton = styled.button`
         cursor: pointer;
         
     }
-
 `;
+
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
+
 
 const Formulario = () => {
 
@@ -49,6 +59,7 @@ const Formulario = () => {
         year: '',
         plan: ''
     });
+    const [ error, guardarError ] = useState(false);
 
     // extraer los valores del state
     const { marca, year, plan } = datos;
@@ -61,8 +72,37 @@ const Formulario = () => {
         })
     }
 
+    // cuando el usuario presiona submit
+    const cotizarSeguro = e => {
+        e.preventDefault();
+        
+        if(marca.trim() === '' || year.trim() === '' || plan.trim() === '') {
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        // obtener la diferencia de años
+
+        // por cada año hay que restar el 3%
+
+        // Americano 15
+        // Asiatico 5%
+        // Europeo 30%
+
+
+        // Basíco aumenta 20%
+        // Completo 50%
+
+        // Total
+
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={cotizarSeguro}
+        >
+            { error ? <Error>Todos los campos son obligatorios</Error> : null }
             <Campo>
                 <Label>Marca</Label>
                     <Select
@@ -118,7 +158,7 @@ const Formulario = () => {
                 /> Completo
             </Campo>
 
-            <Boton type="button">Cotizar</Boton>
+            <Boton type="submit">Cotizar</Boton>
         </form>
     )
 }
