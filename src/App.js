38d59bd492049d20fad299458b0a2a3c7d8aa66f
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-19 11:31:44 -0300

    Creando los Primeros Styled Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Header from './components/Header';
 
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
 
 function App() {
 
   return (
-    <Header 
+    <Contenedor>
+      <Header 
       titulo='Cotizador de Seguros'
     />
+
+    <ContenedorFormulario >
+      
+    </ContenedorFormulario>
+
+
+    </Contenedor>
   );
 }
 
