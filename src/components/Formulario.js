9db2136d761d425ce61915a64a25ed138641ed26
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-08-20 09:28:18 -0300

    Calculando el costo basado en el plan

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { obtenerDiferenciaYear, calcularMarca } from '../helper';
+import { obtenerDiferenciaYear, calcularMarca, obtenerPlan } from '../helper';
 
 const Campo = styled.div`
     display: flex;
@@ -98,12 +98,16 @@ const Formulario = () => {
         // Europeo 30%
         resultado = calcularMarca(marca) * resultado;
 
+
+        // Basíco aumenta 20%
+        // Completo 50%
+        const incrementoPlan = obtenerPlan(plan);
+        resultado = parseFloat( incrementoPlan * resultado ).toFixed(2);
+
         console.log(resultado)
 
 
 
-        // Basíco aumenta 20%
-        // Completo 50%
 
         // Total
 
